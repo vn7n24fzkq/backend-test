70d52846017234bb89842fe7a5c29c8fbefb8ad4
vn7n24fzkq <vn7n24fzkq@gmail.com>
2021-07-31 01:10:54 +0800

    feat(DAO): add user and task DAO

--- dao/task.go
+++ dao/task.go
@@ -1,19 +1,21 @@
 package dao
 
 import (
+	"errors"
 	"time"
 
 	"gorm.io/gorm"
 )
 
 type Task struct {
-	ID        uint      `gorm:"primarykey" json:"id"`
-	Title     string    `json:"title" binding:"required"`
-	Content   string    `json:"content" binding:"required"`
-	ExpiredAt time.Time `json:"expiredAt" binding:"required"`
+	ID        int       `gorm:"primarykey" json:"id"`
+	Title     string    `json:"title" binding:"required" gorm:"not null"`
+	Content   string    `json:"content" binding:"required" gorm:"not null"`
+	ExpiredAt int64     `json:"expiredAt" binding:"required" gorm:"index"`
+	Done      bool      `json:"done" binding:"required"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-	UserID    uint      `json:"UserID"`
+	UserID    int       `json:"UserID" gorm:"index;not null"`
 }
 
 type TaskDAO struct {
@@ -22,4 +24,41 @@ type TaskDAO struct {
 
 func NewTaskDAO(db *gorm.DB) *TaskDAO {
 	return &TaskDAO{db: db}
+}
+
+func (p *TaskDAO) CreateTask(task Task) (Task, error) {
+	result := p.db.Debug().Create(&task)
+	return task, result.Error
+}
+
+// FindOneTask(Task{ID:1})
+func (p *TaskDAO) FindOneTask(condition Task) (Task, error) {
+	var task Task
+	result := p.db.Where(condition).First(&task)
+	if result.Error != nil {
+		return task, result.Error
+	}
+
+	return task, nil
+}
+
+// FindTasks(Task{UserID:1})
+func (p *TaskDAO) FindTasks(condition Task) ([]Task, error) {
+	var tasks []Task
+	result := p.db.Where(condition).Find(&tasks)
+	if result.Error != nil {
+		return tasks, result.Error
+	}
+	return tasks, nil
+}
+
+func (p Task) Update(dao *TaskDAO, task Task) error {
+	if p.ID != task.ID {
+		return errors.New("You can not update th ID")
+	}
+	return dao.db.Save(&task).Error
+}
+
+func (p Task) Delete(dao *TaskDAO) error {
+	return dao.db.Delete(p).Error
 }
--- dao/user.go
+++ dao/user.go
@@ -1,19 +1,20 @@
 package dao
 
 import (
+	"errors"
 	"time"
 
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID             uint      `gorm:"primarykey"`
-	Username       string    `json:"username" binding:"required"`
-	PasswordDigest string    `json:"-"`
-	Salt           string    `json:"-"`
+	ID             int       `gorm:"primarykey"`
+	Username       string    `json:"username" binding:"required" gorm:"not null;size:128;uniqueIndex"`
+	PasswordDigest string    `json:"-" gorm:"not null;size:128"`
+	Salt           string    `json:"-" gorm:"not null;size:128"`
 	CreatedAt      time.Time `json:"createdAt"`
 	UpdatedAt      time.Time `json:"updatedAt"`
-	Tasks          []Task    `json:"-" gorm:"foreignKey:UserID"`
+	Tasks          []Task    `json:"-" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 func NewUserDAO(db *gorm.DB) *UserDAO {
@@ -24,23 +25,29 @@ type UserDAO struct {
 	db *gorm.DB
 }
 
-func (p *UserDAO) CreateUser() {
-	//TODO
+func (p *UserDAO) CreateUser(user User) (User, error) {
+	result := p.db.Create(&user)
+	return user, result.Error
 }
 
-func (p *UserDAO) GetUserByID(id int) (User, error) {
+// FindOneUser(User{ID:1})
+func (p *UserDAO) FindOneUser(condition User) (User, error) {
 	var user User
-	if err := p.db.Model(&user).First(id).Error; err != nil {
-		return user, err
+	result := p.db.Where(condition).First(&user)
+	if result.Error != nil {
+		return user, result.Error
 	}
+
 	return user, nil
-
 }
 
-func (p *UserDAO) UpdateUser() {
-	//TODO
+func (p User) Update(dao *UserDAO, user User) error {
+	if p.ID != user.ID {
+		return errors.New("You can not update th ID")
+	}
+	return dao.db.Save(&user).Error
 }
 
-func (p *UserDAO) DeleteUserByID() {
-	//TODO
+func (p User) Delete(dao *UserDAO) error {
+	return dao.db.Delete(p).Error
 }
